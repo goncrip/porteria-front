8d3cecd55246d30f739be05ffb97a2f1b4f79f1a
goncrip <cristiangonzalezalvarez@gmail.com>
2024-02-25 10:13:11 -0300

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="grid">
+        <div class="col-1">
+
+        </div>
+        <div class="col-10 alignLeft titleBackground p-text-bold">
+            Alta de ingresos
+        </div>
+        <div class="col-1">
+            
+        </div>
+    </div>
+    <div class="grid">
+        <div class="col-1">
+
+        </div>
+        <div class="col-10">
+            <Dropdown v-model="selectedPlate" :options="plates" optionLabel="Matricula" placeholder="Seleccione matricula" filter class="w-full md:w-14rem" />
+        </div>
+        <div class="col-1">
+
+        </div>
+    </div>
+    <div class="grid">
+        <div class="col-1">
+
+        </div>
+        <div class="col-10">
+            <Dropdown v-model="selectedPerson" :options="persons" optionLabel="Documento" placeholder="Seleccione persona" filter class="w-full md:w-14rem" />
+        </div>
+        <div class="col-1">
+
+        </div>
+    </div>
+    <div class="grid">
+        <div class="col-1">
+
+        </div>
+        <div class="col-10">
+            <Dropdown v-model="selectedCompany" :options="companies" optionLabel="Nombre" placeholder="Seleccione empresa" filter class="w-full md:w-14rem" />
+        </div>
+        <div class="col-1">
+
+        </div>
+    </div>
+    <div class="grid">
+        <div class="col-1">
+
+        </div>
+        <div class="col-10">
+            <Dropdown v-model="selectedLoadType" :options="loadTypes" optionLabel="Descripcion" placeholder="Seleccione carga" filter class="w-full md:w-14rem" />
+        </div>
+        <div class="col-1">
+
+        </div>
+    </div>
+    <div class="grid">
+        <div class="col-1">
+
+        </div>
+        <div class="col-10">
+            <InputText type="number" v-model="peso"></InputText>
+        </div>
+        <div class="col-1">
+
+        </div>
+    </div>
+    
+</template>
+<script>
+
+import InputText from 'primevue/inputtext'
+import Dropdown from 'primevue/dropdown';
+
+export default {
+    name: 'App',
+    components:{
+        InputText,
+        Dropdown
+    },
+    data() {
+        return {
+            selectedCompany: 0,
+            selectedPlate: 0,
+            selectedPerson: 0,
+            selectedLoadType: 0,
+            companies: [
+                {
+                    "IdEmpresa": 2,
+                    "Nombre": "Empresa 1"
+                }
+            ],
+            plates:[
+                {
+                    "IdVehiculo": 1,
+                    "Matricula" : "ABC1234",
+                    "Pais": "UY"
+                },
+                {
+                    "IdVehiculo": 2,
+                    "Matricula" : "IAA1234",
+                    "Pais": "UY"
+                },
+                {
+                    "IdVehiculo": 3,
+                    "Matricula" : "IAB5878",
+                    "Pais": "UY"
+                }
+            ],
+            persons:[
+            {
+                    "IdPersona" : 2,
+                    "TipoDocumento": "CI",
+                    "Documento": "45000411",
+                    "Pais": "UY",
+                    "Celular": "099127950",
+                    "Nombres": "Cristian",
+                    "Apellidos": "Gonzalez"
+                },
+                {
+                    "IdPersona" : 3,
+                    "TipoDocumento": "CI",
+                    "Documento": "35040811",
+                    "Pais": "UY",
+                    "Celular": "095124950",
+                    "Nombres": "Pedro",
+                    "Apellidos": "Perez"
+                },
+            ],
+            loadTypes:[
+                {
+                    "IdTipoCarga" : 1,
+                    "Descripcion" : "Trigo"
+                },
+                {
+                    "IdTipoCarga" : 2,
+                    "Descripcion" : "Ladrillo"
+                }
+            ],
+            peso: 0
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,48 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import DataTable from 'primevue/datatable';
+import Dropdown from 'primevue/dropdown';
+import Card from 'primevue/card';
+import Menubar from 'primevue/menubar';
+import Calendar from 'primevue/calendar';
+import Skeleton from 'primevue/skeleton';
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
+import BlockUI from 'primevue/blockui';
+import Dialog from 'primevue/dialog';
+
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+
+import 'primevue/resources/themes/aura-light-green/theme.css';      //theme
+import 'primevue/resources/primevue.min.css';                 //core css
+import 'primeflex/primeflex.css';
+import './styles/common.css';
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.use(ToastService);
+
+app.component('InputText', InputText);
+app.component('DataTable', DataTable);
+app.component('Button', Button);
+app.component('Toast', Toast);
+app.component('Dropdown', Dropdown);
+app.component('Card', Card);
+app.component('Menubar', Menubar);
+app.component('Calendar', Calendar);
+app.component('Skeleton', Skeleton);
+app.component('TabView', TabView);
+app.component('TabPanel', TabPanel);
+app.component('Accordion', Accordion);
+app.component('AccordionTab', AccordionTab);
+app.component('BlockUI', BlockUI);
+app.component('Dialog', Dialog);
+
+app.mount('#app');
